9a10740088f82983e8d8323904544f84f02b7943
Saif Ali <89186408+SaifAliii@users.noreply.github.com>
2023-08-31 01:10:14 +0500

    tried to solve remove elements from leet code

--- RemoveElement.cpp
+++ RemoveElement.cpp
@@ -3,10 +3,38 @@
 using namespace std;
 int removeElement(vector<int>& nums, int val) 
 {
+    int temp, j =1;
+    int i = 0;
+    bool status = false;
+    int size = nums.size();
+    if(size == 1 && nums[i] == val)
+    {
+        return 0;
+    }
+    for(; i < size && j < size ; j++)
+    {
+        if(nums[i] != nums[j])
+        {
+            if(nums[i] == val)
+            {
+                temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                status = true;
+            }
+            i++;
+        }
+    }
+    if(!status)
+    {
+        return 0;
+    }
+    return i;
 
 }
 int main()
 {
-    vetcor<int>
+    vector<int> nums = {3, 3, 3, 2, 2, 2};
+    cout << "\nResult is: " << removeElement(nums, 2);
     return 0;
 }
